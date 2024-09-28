e9630051e72886661601d3a1d46e5d432c15a036
Jk Devloper <141899550+sunil143786@users.noreply.github.com>
2024-09-28 22:21:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '0'))
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Bisalrepo:Bisalrepo@cluster0.tp6ng.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Jaynath143:Jaynath143@cluster0.t0gdpgn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 

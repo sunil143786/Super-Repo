e4a06096fa1886251b93eb5078f97a0968057a1c
Safari <142569922+Safaridevv@users.noreply.github.com>
2024-10-01 22:48:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -16,7 +16,7 @@ from database.ia_filterdb import Media, get_file_details, unpack_new_file_id, ge
 from database.users_chats_db import db
 from database.safari_reffer import sdb
 from info import *
-from utils import get_settings, get_size, is_subscribed, is_req_subscribed, reacts, save_group_settings, temp, get_shortlink, get_seconds
+from utils import get_settings, get_size, is_subscribed, is_req_subscribed, save_group_settings, temp, get_shortlink, get_seconds
 from database.connections_mdb import active_connection
 from utils import react_msg
 import re
